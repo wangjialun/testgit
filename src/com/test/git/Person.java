5a8f735a250cce516512d3098447f9aca4e24680
王加雷 <wangjialei@haohandata.com.cn>
2017-03-14 10:05:15 +0800

    第一次提交

---
+++ src/com/test/git/Person.java
@@ -0,0 +1,17 @@
+package com.test.git;
+
+public class Person {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("²â²â");
+		
+		
+		System.out.println("µÚ¶þ´Î");
+		
+		
+
+	}
+
+}
